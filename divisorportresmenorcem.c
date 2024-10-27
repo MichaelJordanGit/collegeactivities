0eb484f56429d65cd756cb4779a48a458c201ec6
Michael Jordan <39863716+MichaelJordanGit@users.noreply.github.com>
2024-10-27 11:20:16 -0300

    Add files via upload

---
+++ divisorportresmenorcem.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int repetidor;
+    for (repetidor = 3; repetidor < 100; repetidor=repetidor + 3){
+        printf("%d \n", repetidor);
+    }
+    return(0);
+
+}
